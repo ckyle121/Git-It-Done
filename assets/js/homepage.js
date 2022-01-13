9cbab33cb144032945b673fd2939b99eed578a1a
ckyle121 <carolinekyle21@gmail.com>
2022-01-13 14:01:27 -0600

    event listener for searching by username

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,25 @@
+// create variables to select from index.html
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    console.log(event);
+}
+
 // function to search GitHub users and list ofo all their repos tallying open issues for each one 
 var getUserRepos = function(){
     // format the github api url 
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
+    
     // make a request to the url 
     fetch(apiUrl).then(function(response){
+        console.log(response)
         response.json().then(function(data){
             console.log(data);
         });
     });
 }
 
-getUserRepos();
+// add event listener to forms 
+userFormEl.addEventListener("submit", formSubmitHandler)
